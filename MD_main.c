0aa913561b9a4dd8b478b3bcbe122d3694138e14
Josephine <jossan_cuellar@hotmail.com>
2014-11-14 18:01:39 +0100

    Flyttade text till readme

--- MD_main.c
+++ MD_main.c
@@ -22,49 +22,4 @@ int main()
 	/* Initiation of variables */
 	latticeSpacing = 1; //Should be in metal units
     
-    /*
-     Code for generating a uniform random number between 0 and 1. srand should only be called once.
-     */
-    /*
-     srand(time(NULL));
-     double random_value;
-     random_value = (double) rand() / (double) RAND_MAX;
-     */
-    
-    /*
-     Descriptions of the different functions in the files initfcc.c and alpotential.c are listed below.
-     */
-    
-    /* 
-     Function that generates a fcc lattice in units of [Å]. Nc is the number of primitive cells in each direction and a0 is the lattice parameter. The positions of all the atoms are stored in pos which should be a matrix of the size N x 3, where N is the number of atoms. The first, second and third column correspond to the x,y and z coordinate respectively.
-     */
-    /*
-     init_fcc(pos, Nc, a0);
-    */
-    
-    /* 
-     Function that calculates the potential energy in units of [eV]. pos should be a matrix containing the positions of all the atoms, L is the length of the supercell and N is the number of atoms.
-     */
-    /*
-     double energy;
-     energy = get_energy_AL(pos, L, N);
-     */
-    
-    /* 
-     Function that calculates the virial in units of [eV]. pos should be a matrix containing the positions of all the atoms, L is the length of the supercell and N is the number of atoms.
-     */
-    /*
-     double virial;
-     virial = get_virial_AL(pos, L, N);
-     */
-    
-    /*
-     Function that calculates the forces on all atoms in units of [eV/Å]. the forces are stored in f which should be a matrix of size N x 3, where N is the number of atoms and column 1,2 and 3 correspond to the x,y and z component of the force resepctively . pos should be a matrix containing the positions of all the atoms, L is the length of the supercell and N is the number of atoms.
-     */
-    /*
-     get_forces_AL(f,pos, L, N);
-     */
-    
-    
-    
 }
