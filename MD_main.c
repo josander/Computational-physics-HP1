65d977d78ebe3e952b9dbc167d6f0a3fcee15816
Josephine <jossan_cuellar@hotmail.com>
2014-11-16 11:01:02 +0100

    List of what should be done included

--- MD_main.c
+++ MD_main.c
@@ -16,10 +16,37 @@
 int main()
 {
 
-    /* Declaration of variables */
+    	/* Declaration of variables */
 	double latticeSpacing;
+	double timestep;
+	double nbr_of_timesteps;
+	double nbr_of_atoms;
+	double energy;
+	double pe;
+	double ke;
+
 
 	/* Initiation of variables */
 	latticeSpacing = 1; //Should be in metal units
+	timestep = 1;
+	nbr_of_timesteps = 1000;
+	nbr_of_atoms = 256;
+
+
+	// Initiate a fcc lattice of Al-particles
+
+	// Introduce displacements, about 5 % of the lattice spacing
+
+	// Set initial velocities to 0 
+
+	// Save the time evolution of the pe, ke and total energy
+	
+	/* Print energy data to output file */
+	FILE *e_file;
+	e_file = fopen("energy.data","w");
+
+	fprintf(e_file,"%.5f \t %e \t %e \n", energy, pe, ke);
+
+	fclose(e_file);
     
 }
