eca31c98ba34c6a38b3afb4e7fc1b3509c3cbbeb
David <david.emil.svensson@gmail.com>
2014-11-16 18:10:48 +0100

    plot.m

--- plot_func.m
+++ plot_func.m
@@ -3,17 +3,30 @@
 % load the data file
 clf
 data = importdata('energy.data');
-%%
-
 set(gcf,'renderer','painters','PaperPosition',[0 0 12 6]);
 
+%%
+
+
 %plot
-hold on
-plot(data(:,1),data(:,2:end));
+
+plot(data(:,1),data(:,2:end-2));
 
 
 % labels
 ylabel('Energy');
 xlabel('Time');
-print(gcf,'-depsc2','samp2.eps')
+print(gcf,'-depsc2','energy.eps')
+%% 
+plot(data(:,1),data(:,end-1));
 
+ylabel('Temerature');
+xlabel('Time');
+print(gcf,'-depsc2','temperature.eps')
+
+%% 
+plot(data(:,1),data(:,end));
+
+ylabel('Pressure');
+xlabel('Time');
+print(gcf,'-depsc2','pressure.eps')
