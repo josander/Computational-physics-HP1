5cde61e7d6204cd94fe168f76befec251c814e90
Josephine <jossan_cuellar@hotmail.com>
2014-11-16 14:11:37 +0100

    Verlet algoritm ok for [nbr of pert][3]

--- MD_main.c
+++ MD_main.c
@@ -24,23 +24,21 @@ int main()
 	int nbr_of_atoms;
 	int Nx, Ny, Nz;
      	double random_value;
-	int i, j;
+	int i, j, n;
 	double m;
+	double energy, pe, ke;
 
 	// Initiation of variables 
 	lattice_param = 4.05; // Units: [Å]
-	timestep = 0.1;
+	timestep = 0.1; // 
 	nbr_of_timesteps = 1000;
 	nbr_of_atoms = 256;
 	Nx = 4, Ny = 4, Nz = 4;
-	m = 0.00279636665; // metal units
+	m = 0.00279636665; // Metal units
 
 	// Declaration of matrixes and arrays 
 	double q[4*Nx*Ny*Nz][3], v[nbr_of_atoms][3], a[nbr_of_atoms][3];
 	double f[4*Nx*Ny*Nz][3];
-	double energy[nbr_of_atoms];
-	double pe[nbr_of_atoms];
-	double ke[nbr_of_atoms];
 
 	// Initiation of the fcc lattice of Al-atoms 
 	init_fcc(q, Nx, lattice_param);
@@ -64,40 +62,10 @@ int main()
 		}
 	}
 
-	// Calculation of initial energies
-	//get_energy_AL(q, Nx*lattice_param, nbr_of_atoms);
-	//calc_ke();
-	//energy = pe + ke;
-
-	// Time evolution according to the velocity Verlet algorithm
-	for (i = 1; i < nbr_of_timesteps + 1; i++){
-		// v(t+dt/2)
-		for (j = 0; j < nbr_of_atoms; j++){
-		    v[j] += timestep * 0.5 * a[j];
-		} 
-
-		// q(t+dt) 
-		for (j = 0; j < nbr_of_atoms; j++){
-		    q[j] += timestep * v[j];
-		}
-
-		// a(t+dt)
-		//calc_acc(a, q, m, kappa, nbr_of_atoms);
-
-		// v(t+dt)
-		for (j = 0; j < nbr_of_atoms; j++){
-		    v[j] += timestep * 0.5 * a[j];
-		} 
-
-
-		// Calcutaion of the pe, ke and total energy
-		// calc_pe();
-		// calc_ke();
-		// energy = pe + ke;
-	
-		// Print the average energy data to output file
-		fprintf(e_file,"%.5f \t %e \t %e \t %e \n", i*timestep, energy, pe, ke);
-	}
+	// Calculate of initial energies
+	get_energy_AL(q, Nx*lattice_param, nbr_of_atoms);
+	get_ke(v, nbr_of_atoms, m);
+	energy = pe + ke;
 
 	// Make a file to save the energies in
 	FILE *e_file;
@@ -105,6 +73,49 @@ int main()
 
 	// Save the initial energies in the file
 	fprintf(e_file,"%.5f \t %e \t %e \t %e \n", 0.0, energy, pe, ke);
+
+	// Time evolution according to the velocity Verlet algorithm
+	for (i = 1; i < nbr_of_timesteps + 1; i++){
+		// v(t+dt/2)
+		for (j = 0; j < nbr_of_atoms; j++){
+			for(n = 0; n < 3; n++){
+		    		v[j][n] += timestep * 0.5 * a[j][n];
+			}
+		} 
+
+		// q(t+dt) 
+		for (j = 0; j < nbr_of_atoms; j++){
+			for(n = 0; n < 3; n++){
+		    		q[j][n] += timestep * v[j][n];
+			}
+		}
+
+		// a(t+dt)
+		// Get forces
+		get_forces_AL(f, q, Nx*lattice_param, nbr_of_atoms);
+
+		// Scale forces to acceleration
+		for(i = 0; i < nbr_of_atoms; i++){
+			for(j = 0; j < 3; j++){
+				a[i][j] = f[i][j]/m;
+			}
+		}
+
+		// v(t+dt)
+		for (j = 0; j < nbr_of_atoms; j++){
+			for(n = 0; n < 3; n++){
+		    		v[j][n] += timestep * 0.5 * a[j][n];
+			}
+		} 
+
+		// Calcutaion of the pe, ke and total energy
+		get_energy_AL(q, Nx*lattice_param, nbr_of_atoms);
+		get_ke(v, nbr_of_atoms, m);
+		energy = pe + ke;
+	
+		// Print the average energy data to output file
+		fprintf(e_file,"%.5f \t %e \t %e \t %e \n", i*timestep, energy, pe, ke);
+	}
 
 	// Close the energy output file 
 	fclose(e_file);
