d0e2ba79b5f512297a9559617472bbfcb82f08e5
Josephine <jossan_cuellar@hotmail.com>
2014-11-16 13:51:58 +0100

    Main ändringar

--- MD_main.c
+++ MD_main.c
@@ -25,6 +25,7 @@ int main()
 	int Nx, Ny, Nz;
      	double random_value;
 	int i, j;
+	double m;
 
 	// Initiation of variables 
 	lattice_param = 4.05; // Units: [Å]
@@ -32,9 +33,11 @@ int main()
 	nbr_of_timesteps = 1000;
 	nbr_of_atoms = 256;
 	Nx = 4, Ny = 4, Nz = 4;
+	m = 1; // Units: [u/...]
 
 	// Declaration of matrixes and arrays 
 	double q[4*Nx*Ny*Nz][3];
+	double f[4*Nx*Ny*Nz][3];
 	double v[nbr_of_atoms], a[nbr_of_atoms];
 	double energy[nbr_of_atoms];
 	double pe[nbr_of_atoms];
@@ -45,37 +48,65 @@ int main()
 
 	// Introduction of displacements, about 5 % of the lattice spacing
      	srand(time(NULL)); // Should only be called once
-     	//random_value = (double) rand() / (double) RAND_MAX;
-
-	
 	rand_disp(q, lattice_param, nbr_of_atoms);
-	/*
-	for(i = 0; i < 10; i++){
-		printf("%F \t %F \t %F \n", q[i][0], q[i][1], q[i][2]);
-	}
-	*/
+
 	// Initiation of the velocities 
 	for(i = 0; i < nbr_of_atoms; i++){
 		v[i] = 0.0;
 	}
 
-	// Use the Al-potential to calculate the initial acceleration
+	// Get forces
+	get_forces_AL(f, q, Nx*lattice_param, nbr_of_atoms);
 
-	/* Calculation of initial energies */
+	// Scale forces to acceleration
+	
+
+	// Use the Al-potential to calculate the initial acceleration
+	
+
+	// Calculation of initial energies
 	//get_energy_AL(q, Nx*lattice_param, nbr_of_atoms);
 	//calc_ke();
 	//energy = pe + ke;
 
-	/* Make a file to save the energies in*/
+	// Time evolution according to the velocity Verlet algorithm
+	for (i = 1; i < nbr_of_timesteps + 1; i++){
+		// v(t+dt/2)
+		for (j = 0; j < nbr_of_atoms; j++){
+		    v[j] += timestep * 0.5 * a[j];
+		} 
+
+		// q(t+dt) 
+		for (j = 0; j < nbr_of_atoms; j++){
+		    q[j] += timestep * v[j];
+		}
+
+		// a(t+dt)
+		//calc_acc(a, q, m, kappa, nbr_of_atoms);
+
+		// v(t+dt)
+		for (j = 0; j < nbr_of_atoms; j++){
+		    v[j] += timestep * 0.5 * a[j];
+		} 
+
+
+		// Calcutaion of the pe, ke and total energy
+		// calc_pe();
+		// calc_ke();
+		// energy = pe + ke;
+	
+		// Print the average energy data to output file
+		fprintf(e_file,"%.5f \t %e \t %e \t %e \n", i*timestep, energy, pe, ke);
+	}
+
+	// Make a file to save the energies in
 	FILE *e_file;
 	e_file = fopen("energy.data","w");
 
-	/* Save the initial energies in the file*/
+	// Save the initial energies in the file
 	fprintf(e_file,"%.5f \t %e \t %e \t %e \n", 0.0, energy, pe, ke);
 
-
-
-	/* Close the energy output file */
+	// Close the energy output file 
 	fclose(e_file);
 
 	return 0;
