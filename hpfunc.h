115f28891ec3b63fe0e23e3f57b748fea90fa9ef
David <david.emil.svensson@gmail.com>
2014-11-16 12:06:59 +0100

    la till hpfunc med randdisp

---
+++ hpfunc.h
@@ -0,0 +1,16 @@
+/*
+hpfunc.h
+ 
+.
+*/
+
+#ifndef _hpfunc_h
+#define _hpfunc_h
+
+//extern void get_forces_AL(double[][3] , double[][3], double, int);
+//extern double get_energy_AL(double[][3], double, int);
+//extern double get_virial_AL(double[][3], double, int);
+extern void rand_disp(double[][3],double, int);
+
+
+#endif
