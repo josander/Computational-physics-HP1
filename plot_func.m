774c77fb301bce0b2c68f09ac89a061aec13aaff
David <david.emil.svensson@gmail.com>
2014-11-20 15:53:49 +0100

    fixed corr plot

--- plot_func.m
+++ plot_func.m
@@ -48,22 +48,44 @@ sT= 5.028783;
 sP= 4.884246; 
 
 %% Plot the correlation data
-corrSamp = 10000;
+corrSamp = 10000;  % The maximum value for k to be plotted, [0.01ps]
 figure(4);
 corrData = importdata('correlation.data');
-set(gcf,'renderer','painters','PaperPosition',[0 0 12 6]);
+%%
+clf
+set(gcf,'renderer','painters','PaperPosition',[0 0 4.7 3]);
 Size = size(corrData);
 
-subplot(2,1,1);
 plot((0:0.01:(corrSamp-0.1)/100)',corrData(1:corrSamp,1));
 hold on
 plot([0 (corrSamp-0.01)/100], [exp(-2) exp(-2)],'g-');
+%axis([0 100 -0.4 0.4])
+title('Correlation function for temperature','interpreter','latex','fontsize',14);
+y = ylabel('$\Phi _T (k)$ [-]','interpreter','latex','fontsize',10);
+x = xlabel('Time lag $k$ [ps]','interpreter','latex','fontsize',10);
+%s4et(x, 'Units', 'Normalized', 'Position', [0.5, 0, 0]);
+hold off
+plotTickLatex2D
+set(y, 'Units', 'Normalized', 'Position', [-0.1, 0.5, 0]);
+l = legend('Correlation function','$ e^{-2}$');
+set(l,'Interpreter','latex');
+print(gcf,'-depsc2','correlationT.eps')
 
-subplot(2,1,2);
 plot((0:0.01:(corrSamp-0.1)/100)',corrData(1:corrSamp,2));
 hold on
 plot([0 (corrSamp-0.01)/100], [exp(-2) exp(-2)],'g-');
+%axis([0 100 0 0.4])
+title('Correlation function for preassure','interpreter','latex','fontsize',14);
+y = ylabel('$\Phi _P (k)$ [-]','interpreter','latex','fontsize',10);
+x = xlabel('Time lag $k$ [ps]','interpreter','latex','fontsize',10);
+%set(x, 'Units', 'Normalized', 'Position', [0.5, -0.01, 0]);
+hold off
+plotTickLatex2D
+set(y, 'Units', 'Normalized', 'Position', [-0.1, 0.5, 0]);
+l = legend('Correlation function','$ e^{-2}$');
+set(l,'Interpreter','latex');
 
+print(gcf,'-depsc2','correlationP.eps')
 %% Plot the displacement in 3D
 
 figure(5);
